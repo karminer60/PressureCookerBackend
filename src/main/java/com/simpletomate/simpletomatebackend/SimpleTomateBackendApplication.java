3eaa6461d67cd16460e9c90bbc10c9c79b8c0c54
karminer60 <karminer60@gmail.com>
2023-02-08 23:51:55 -0800

    spring boot application runs correctly

--- src/main/java/com/simpletomate/simpletomatebackend/SimpleTomateBackendApplication.java
+++ src/main/java/com/simpletomate/simpletomatebackend/SimpleTomateBackendApplication.java
@@ -2,12 +2,13 @@ package com.simpletomate.simpletomatebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SimpleTomateBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SimpleTomateBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTomateBackendApplication.class, args);
+	}
 
 }
--- src/test/java/com/simpletomate/simpletomatebackend/SimpleTomateBackendApplicationTests.java
+++ src/test/java/com/simpletomate/simpletomatebackend/SimpleTomateBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SimpleTomateBackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
