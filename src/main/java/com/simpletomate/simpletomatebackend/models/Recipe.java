e0ad1382dd9176f7081dbf997e9811895c621b79
karminer60 <karminer60@gmail.com>
2023-02-10 01:17:18 -0800

    adds template code for simple controller and model

---
+++ src/main/java/com/simpletomate/simpletomatebackend/models/Recipe.java
@@ -0,0 +1,79 @@
+package com.simpletomate.simpletomatebackend.models;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "coins")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy  = GenerationType.AUTO)
+    Long coinid;
+    String name;
+    String namepural;
+    double value;
+    int quantity;
+
+    public Recipe(String name, String namepural, double value, int quantity) {
+        this.name = name;
+        this.namepural = namepural;
+        this.value = value;
+        this.quantity = quantity;
+    }
+
+    public Recipe() {
+        //default constructor
+    }
+
+    public long getCoinid() {
+        return coinid;
+    }
+
+    public void setCoinid(long coinid) {
+        this.coinid = coinid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNamepural() {
+        return namepural;
+    }
+
+    public void setNamepural(String namepural) {
+        this.namepural = namepural;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Coin{" +
+                "coinid=" + coinid +
+                ", name=" + name +
+                ", namepural=" + namepural +
+                ", value=" + value +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
+
--- src/main/java/com/simpletomate/simpletomatebackend/repositories/SimpleTomateBackendApplication.java
+++ src/main/java/com/simpletomate/simpletomatebackend/repositories/SimpleTomateBackendApplication.java
@@ -1,4 +1,4 @@
-package com.simpletomate.simpletomatebackend;
+package com.simpletomate.simpletomatebackend.repositories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
