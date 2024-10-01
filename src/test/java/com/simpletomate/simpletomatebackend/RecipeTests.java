3023be25d315853a377faa29b3a88c8bf3730a1a
karminer60 <karminer60@gmail.com>
2024-09-30 17:23:44 -0700

    basic outlines for tests

---
+++ src/test/java/com/simpletomate/simpletomatebackend/RecipeTests.java
@@ -0,0 +1,19 @@
+
+package com.simpletomate.simpletomatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    public void createRecipe(){
+
+    }
+
+
+}
