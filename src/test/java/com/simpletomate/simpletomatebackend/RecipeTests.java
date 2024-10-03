61998cdab503eb69eb9bd0428859fc708ab396f0
karminer60 <karminer60@gmail.com>
2024-10-02 19:08:23 -0700

    adds an integration test for the createRecipe post method

--- src/test/java/com/simpletomate/simpletomatebackend/RecipeTests.java
+++ src/test/java/com/simpletomate/simpletomatebackend/RecipeTests.java
@@ -1,19 +1,38 @@
 
 package com.simpletomate.simpletomatebackend;
 
+import com.simpletomate.model.Recipe;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class RecipeTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
     void contextLoads() {
     }
 
-    public void createRecipe(){
 
+
+    public void shouldReturn201ResponseIncludingLocationHeaderAndCreateRecipeInOurDatabase() {
+
+        final Recipe newRecipe = new Recipe(null, "Rowan Atkinson", true);
+
+        final ResponseEntity<String> response = template.postForEntity(String.format("http://localhost:%d/api/customers", port), newRecipe, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNull();
+        assertThat(response.getHeaders().getLocation().toString()).isEqualTo(String.format("http://localhost:%d/api/customers/0", port));
     }
-
 
 }
